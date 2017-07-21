a7a34b02a66b34969b3db4c696d3d17336f07d5e
Praveen sharma <sharma.praveenkumar10@gmail.com>
2017-07-21 14:52:43 +0530

    add

--- learn.php
+++ learn.php
@@ -6,3 +6,8 @@ sentance 2.........git hub changes 1
 sentence 3/////////dtop
 
 sentence 4/////////hello
+
+Changes to be commChanges to be comm
+Changes to be comm
+Changes to be comm
+Changes to be comm
